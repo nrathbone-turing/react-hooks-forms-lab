33cf21f308131e9719655bcae3301db4829f1718
Nick Rathbone <nrathbone@joinhandshake.com>
2025-07-12 16:10:28 -0600

    Refactor ItemForm file to have controlled inputs and proper form submission/reset logic

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -2,16 +2,48 @@ import React from "react";
 import { v4 as uuid } from 'uuid';
 
 function ItemForm(props) {
+  // Controlled input states
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("Produce"); //Defaults to Produce
+  
+  // event handler for form submission
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    const newItem = {
+      id: uuid(),
+      name: name,
+      category: category,
+    };
+
+    onItemFormSubmit(newItem); 
+
+    // Reset form after submission
+    setName("");
+    setCategory("Produce");
+  }
+  
+  
+  
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={handleSubmit}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input 
+          type="text" 
+          name="name"
+          value={name} // controlled
+          onChange={(e) => setName(e.target.value)} // updates state
+        />
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select 
+          name="category"
+          value={category} // controlled
+          onChange={(e) => setCategory(e.target.value)} // updates state
+        >
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
