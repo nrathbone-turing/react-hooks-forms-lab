ba11c9f5bcd830785866807a3ed48e02dff72541
Nick Rathbone <nrathbone@joinhandshake.com>
2025-07-12 16:04:02 -0600

    add event handler function for onChange typing behavior to udpate results filtered

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,26 @@
 import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import Header from "./Header";
+import Filter from "./Filter";
 import itemData from "../data/items";
 
 function App() {
   const [items, setItems] = useState(itemData);
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [searchText, setSearchText] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  function handleCategoryChange(e) {
+    setSelectedCategory(e.target.value);
+  }
+
+  const filteredItems = items
+    .filter((item) =>
+      item.name.toLowerCase().includes(searchText.toLowerCase())
+    )
+    .filter((item) =>
+      selectedCategory === "All" ? true : item.category === selectedCategory
+    );
 
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
@@ -14,7 +29,12 @@ function App() {
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={filteredItems} />
+      <Filter
+        searchText={searchText}
+        onSearchChange={setSearchText}
+        onCategoryChange={handleCategoryChange}
+      />
     </div>
   );
 }
